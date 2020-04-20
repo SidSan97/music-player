1052d61204ea9741e0c08f77ba19904515b5a325
SidSan97 <sidnei1.8santiago@hotmail.com>
2020-04-20 17:22:08 -0300

    Estilização do main menu

--- index.php
+++ index.php
@@ -14,7 +14,32 @@
 
     <title>SidSan Music</title>
 </head>
-<body>
+
+ <body style="background-color: black;">
+     <section class="menu-principal">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#"> SidSan Beats</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">                   
+                    <div class="form-inline my-2 my-lg-0 ml-auto">
+                        <span class="info-menu">
+                            <a class="mr-2">Video</a>
+                            <a>Contato</a>
+                        </span>
+
+                        <button class="my-2 my-sm-0 ml-3 btn-car"><i class="fas "></i>Carrinho</button>
+                        <button class="my-2 my-sm-0 ml-3 mr-2 btn-login"><i class="fas "></i>Login</button>
+                    </div>                                 
+                </div>
+            </nav>
+     </section>
+
+     <section class="imgbg">
+        
+     </section>
     
     <script src="js/jquery-3.3.1.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
@@ -22,5 +47,5 @@
     <script src="js/bootstrap.js"></script>
     <script src="js/script.js"></script>
     <script src="js/bootstrap.min.js"></script>
-</body>
+ </body>
 </html>
